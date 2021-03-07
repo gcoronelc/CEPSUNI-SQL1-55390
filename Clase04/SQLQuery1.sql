9a0226c36e607ee33c59bea6d294b10da6a0e824
Gustavo Coronel <gcoronelc@gmail.com>
2021-03-06 20:54:42 -0500

    Clase 04

---
+++ Clase04/SQLQuery1.sql
@@ -0,0 +1,145 @@
+﻿-- SENTENCIA: SELECT
+/*
+
+SINTAXIS:
+
+	SELECT [TOP n] <lista de columnas>
+	FROM <tabla>
+	WHERE <filtro de filas>
+	GROUP BY <columnas de agrupamiento>
+	HAVING <filtro de grupos>
+	ORDER BY <columnas de ordenamiento>
+
+*/
+
+-- Ejemplo 1
+
+-- <base de datos>.<esquema>.<tabla>
+
+SELECT * FROM EDUCA.DBO.CURSO;
+GO
+
+SELECT * FROM CURSO;
+GO
+
+
+-- Ejemplo 2
+
+SELECT cur_id, cur_nombre FROM curso;
+go
+
+-- Ejemplo 3
+
+SELECT cur_id codigo, cur_nombre nombre
+FROM curso;
+go
+
+-- Selección de columnas
+
+-- Ejercicio 1. 
+/*
+Desarrolle una sentencia SELECT para consultar el nombre, apellido y correo de los
+empleados de la base de datos RH.
+*/
+
+-- De Elena Del Castillo para todos:  03:13 PM
+
+select nombre, apellido, email from RH.dbo.empleado;go
+
+
+-- Ejercicio 2
+/*
+Desarrolle una sentencia SELECT para consultar el nombre, vacantes y precio de cada curso
+de la base de datos EDUCA.
+*/
+
+-- De Rossmery Tolentino para todos:  03:11 PM
+
+SELECT CUR_nombre nombre,cur_vacantes,cur_precio
+from EDUCA.DBO.CURSO;
+go
+
+-- CAMPOS CALCULADOS
+
+select c.*, (cur_vacantes - cur_matriculados) "Vacantes Disponibles"
+from educa.dbo.curso c;
+go
+
+
+--Ejercicio 3. 
+/*
+Desarrollar una sentencia SELECT que permita obtener el importe que se obtendría en cada
+curso, si se logra vender todas las vacantes por cada curso. Base de datos EDUCA.
+*/
+
+-- De Francesca Flores para todos:  03:35 PM
+SELECT X. * , (cur_vacantes*cur_precio) "importe total"FROM EDUCA.dbo.CURSO X;GO
+
+-- De Mariam Ipanaqué para todos:  03:38 PM
+SELECT M.*, (cur_vacantes*cur_precio) "Importe"FROM EDUCA.DBO.CURSO M;GO
+--De Elena Del Castillo para todos:  03:41 PM
+select c.*, (cur_vacantes * cur_precio) "Importe Total"from educa.dbo.curso c;
+GO
+
+
+-- Ejercicio 4.
+/*
+Desarrollar una consulta que permita mostrar las vacantes programas, los matriculados y las
+vacantes disponibles de cada curso de la base de datos EDUTEC.
+*/
+
+SELECT * FROM EDUtec.dbo.CursoProgramado;
+GO
+
+-- De Rossmery Tolentino para todos:  03:37 PM
+select 	CP.*,	(vacantes+matriculados )"vacantes programadas", matriculados,vacantesfrom EDUtec.dbo.CursoProgramado CP;go
+
+
+
+
+-- Ejercicio 5. 
+/*
+El área de recursos humanos necesita una consulta que permita mostrar de cada empleado
+su sueldo actual y cuanto sería su incremento si se proyecta un aumento del 20% a cada uno
+respectivamente.
+*/
+
+
+
+-- CONCATENACION
+
+SELECT CONCAT('GUSTAVO',' CORONEL',' CASTILLO') "NOMBRE COMPLETO";
+GO
+
+
+-- Ejercicio 6
+-- De Elena Del Castillo para todos:  03:56 PM
+select CONCAT(apellido, ' ' ,nombre) from rh.dbo.empleado;
+GO
+
+
+
+-- De Rodrigo Gutarra para todos:  04:00 PM
+-- Ejercicio 8:   select (idcursoProg) "CODIGO", (CONCAT(IdCiclo, '/' ,IdCurso)) "CURSO",(CONCAT(Matriculados, '/' ,(Vacantes+Matriculados)))"VACANTES"from EduTec.dbo.CursoProgramado;
+GO
+
+
+-- De Francesca Flores para todos:  04:00 PM
+-- EJERCICIO 7SELECT CONCAT (NomProfesor,' ',ApeProfesor) "NOMBRES COMPLETOS"from EduTec.dbo.Profesor;
+GO
+
+
+-- De Mariam Ipanaqué para todos:  04:00 PM
+-- Ejercicio 6
+
+SELECT CONCAT (apellido, ' ' ,nombre) from RH.DBO.EMPLEADO;GO
+
+
+-- De Rodrigo Gutarra para todos:  04:01 PM
+-- Ejercicio 5:    SELECt 	idempleado,apellido, nombre, sueldo, 	(sueldo*1.20)"Incremento"from rh.dbo.empleado;
+go
+
+
+-- De Mariam Ipanaqué para todos:  04:24 PM
+-- Ejercicio 8: select IdCursoProg codigo, concat(IdCiclo,'/',IdCurso) curso,concat(Matriculados,'/',(Vacantes + matriculados))  vacantesfrom EduTec.dbo.CursoProgramado;go
+
---
+++ Clase04/SQLQuery2.sql
@@ -0,0 +1,198 @@
+-- FILTRO DE FILAS
+
+-- Ejemplo 1
+
+select * 
+from rh.dbo.empleado
+where sueldo < 2500;
+go
+
+-- Ejemplo 2
+
+select * 
+from edutec.dbo.CursoProgramado
+where IdProfesor = 'P007'and IdCiclo = '2021-03';
+go
+
+select count(1) from edutec.dbo.CursoProgramado;
+go
+
+select count(1) from edutec.dbo.Matricula;
+go
+
+
+-- De Rossmery Tolentino para todos:  04:51 PM
+-- Ejercicio 9
+select *from rh.dbo.empleadowhere iddepartamento=101;go
+
+-- De Elena Del Castillo para todos:  04:51 PM
+-- Ejercicio 9
+select * from rh.dbo.empleado 
+where iddepartamento = 101;
+go
+
+
+-- De Francesca Flores para todos:  04:52 PM
+-- Ejercicio 10
+SELECT *FROM RH.DBO.empleadoWHERE idcargo = 'C02';go
+
+-- De Mariam Ipanaqué para todos:  04:54 PM
+-- Ejercicio 9
+select Idempleado from rh.dbo.empleado where iddepartamento = 101;go-- De Rodrigo Gutarra para todos:  04:54 PM
+-- Ejercicio 10: SELECT idempleado, apellido, nombre, idcargoFROM RH.dbo.empleadoWHERE idcargo ='C01' or idcargo = 'C02';GO
+
+
+
+-- OPERADORES LOGICOS
+-- AND, OR y NOT
+
+
+-- Ejemplo
+
+SELECT *
+FROM RH.DBO.empleado
+WHERE iddepartamento = 100  OR  iddepartamento = 105;
+GO
+
+-- De Mariam Ipanaqué para todos:  05:07 PM
+-- Ejercicio 12: select idempleado, sueldofrom rh.dbo.empleadowhere iddepartamento = 101 AND sueldo > 10000;go
+
+
+-- De Rossmery Tolentino para todos:  05:10 PM
+select * from rh.dbo.empleadowhere iddepartamento=101 AND sueldo>10000;go
+
+
+-- De Elena Del Castillo para todos:  05:11 PM
+select * from rh.dbo.empleado where iddepartamento=101 and sueldo>10000;
+GO
+
+-- De Rodrigo Gutarra para todos:  05:12 PM
+-- Ejercicio 13: SELECT *FROM EDUCA.DBO.CURSOWHERE cur_profesor = ' ' OR cur_profesor is Null;GO
+
+-- LIKE
+/*
+
+SINTAXIS
+
+	<campo> [NOT] LIKE 'patron'
+
+Caracteres comodines:
+
+	_  =>  Cualquier caracter
+	%  =>  Cadena de caracteres
+
+*/
+
+-- Ejemplo
+
+select * 
+from rh.dbo.empleado
+where nombre like 'O%';
+go
+
+-- Ejemplo
+
+select * 
+from rh.dbo.empleado
+where nombre like '%a';
+go
+
+-- Ejemplo
+
+select * 
+from rh.dbo.empleado
+where nombre not like '_[AO]%';
+go
+
+
+-- BETWEEN
+/*
+
+SINTAXIS
+
+	<columna> [NOT] BETWEEN <valor1> AND <valor2>
+
+*/
+
+
+-- Ejemplo
+
+
+select * 
+from rh.dbo.empleado
+WHERE sueldo between 3000 and 7000;
+go
+
+select * 
+from rh.dbo.empleado
+WHERE sueldo not  between 3000 and 7000;
+go
+
+-- IN
+/*
+
+SINTAXIS
+
+	<columna> [NOT] IN ( <valor1>, <valor2>, ... )
+
+*/
+
+-- De Rodrigo Gutarra para todos:  04:54 PM
+-- Ejercicio 10: SELECT idempleado, apellido, nombre, idcargoFROM RH.dbo.empleadoWHERE idcargo ='C01' or idcargo = 'C02';GOSELECT idempleado, apellido, nombre, idcargoFROM RH.dbo.empleadoWHERE idcargo IN ( 'C01', 'C02' );GO-- De Mariam Ipanaqué para todos:  05:35 PM
+-- Ejercicio 14
+
+select nombrefrom rh.dbo.empleadoWHERE NOMBRE like '%O';go
+-- De Rodrigo Gutarra para todos:  05:36 PM
+-- Ejercicio 15 : SELECT * FROM RH.DBO.EMPLEADOWHERE apellido like '_[AO]%';GO
+
+
+-- De Mariam Ipanaqué para todos:  05:38 PM
+-- PREGUNTA 17select nombre, iddepartamentofrom rh.dbo.empleadoWHERE iddepartamento IN (101, 103);GO
+
+-- De Mariam Ipanaqué para todos:  05:38 PM
+-- pregunta 18select nombre, apellido, fecingresofrom rh.dbo.empleadoorder by fecingreso asc;go
+
+
+-- De Elena Del Castillo para todos:  05:39 PM
+-- Ejercicio 16
+
+SELECT * FROM RH.DBO.empleadoWHERE sueldo BETWEEN 3001 AND 9999;
+
+-- De Rodrigo Gutarra para todos:  05:40 PM
+-- Ejercicio 16
+
+SELECT * FROM RH.DBO.empleadoWHERE sueldo > 3000 AND SUELDO < 10000;GO
+
+
+-- NULOS
+/*
+Cualquier operación con un valor NULL es otro valor NULL.
+*/
+
+-- Ejemplo
+select 100 + null "respuest";
+go
+
+-- ISNULL
+
+select isnull(200, 500) Resultado;
+go
+
+select isnull(null, 500) Resultado;
+go
+
+
+-- FECHAS
+/*
+
+DATEPART
+
+	Retorna una parte de la fecha.
+
+*/
+
+
+select e.*, DATENAME(dw,fecingreso) "Dia Semana"
+from rh.dbo.empleado e;
+go
+
