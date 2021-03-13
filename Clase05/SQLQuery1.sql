6a9db6beb51f2b9f55b959e8f46744e47444184d
Gustavo Coronel <gcoronelc@gmail.com>
2021-03-13 15:39:33 -0500

    Clase 05

--- Clase05/SQLQuery1.sql
+++ Clase05/SQLQuery1.sql
@@ -40,4 +40,59 @@ go
 
 
 
+-- De Elena Del Castillo para todos:  03:09 PM
+-- Ejercicio 1
+
+select sum(sueldo + isnull(comision,0)) from dbo.empleado 
+where idcargo = 'C05';
+go
+
+-- Ojo, es departamento de ventas.
+
+select sum(sueldo + isnull(comision,0)) from dbo.empleado 
+where iddepartamento = 103;
+go
+
+
+-- De Francesca Flores para todos:  03:14 PM
+-- Ejercicio 2
+
+select 	MAX(sueldo) [MAYOR SUELDO],	MIN (sueldo) [Menor Sueldo]from RH.dbo.empleadowhere iddepartamento = 103;GO
+
+
+-- GROUP BY
+/*
+Desarrolle una sentencia SELECT para encontrar los departamentos 
+que tienen por lo menos un trabajador. 
+Base de datos RH.
+*/
+
+select distinct iddepartamento 
+from rh.dbo.empleado;
+go
+
+-- Ejercicio
+/*
+Desarrollar una sentencia SELECT que permita obtener el importe de lo 
+recaudado hasta el momento de los cursos vendidos. 
+Base de datos EDUCA.
+*/
+
+select sum(pag_importe) recaudado
+from educa.dbo.PAGO;
+go
+
+/*
+La informacion se necesita por curso
+*/
+
+select cur_id, sum(pag_importe) recaudado
+from educa.dbo.PAGO
+group by cur_id;
+go
+
+
+
+
+
 
